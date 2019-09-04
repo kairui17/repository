d63cf3ccdfdb90c864de5305657483976c83d270
ruirui <516695938@qq.com>
2019-09-04 15:37:32 +0800

    第一次

---
+++ src/cn/kgc/Demo.java
@@ -0,0 +1,11 @@
+package cn.kgc;
+
+/**
+ * @author liurui
+ * @date $ {DATE} 15:36
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("asdfasdf");
+    }
+}
